d30e449ef0ce905aae72396b75bcaa759532a0d1
Jereme Allen <jereme.allen@wheniwork.com>
2017-04-11 17:32:39 -0500

    Add in vuex state

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import store from './vuex/store';
 import App from './App';
 
 Vue.config.productionTip = false;
@@ -10,4 +11,5 @@ new Vue({
   el: '#app',
   template: '<App/>',
   components: { App },
+  store,
 });
---
+++ src/vuex/store.js
@@ -0,0 +1,49 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+// the root, initial state object
+const state = {
+  notes: [],
+  activeNote: {},
+};
+
+// define the possible mutations that can be applied to our state
+const mutations = {
+  ADD_NOTE(appState) {
+    const theState = appState;
+    const newNote = {
+      text: 'New note',
+      favorite: false,
+    };
+
+    // only mutators can mutate the state
+    theState.notes.push(newNote);
+    theState.activeNote = newNote;
+  },
+  EDIT_NOTE(appState, text) {
+    const theState = appState;
+    theState.activeNote.text = text;
+  },
+  DELETE_NOTE(appState) {
+    const theState = appState;
+    state.notes.$remove(state.activeNote);
+    theState.activeNote = state.notes[0];
+  },
+  TOGGLE_FAVORITE(appState) {
+    const theState = appState;
+    theState.activeNote.favorite = !state.activeNote.favorite;
+  },
+  SET_ACTIVE_NOTE(appState, note) {
+    const theState = appState;
+    theState.activeNote = note;
+  },
+};
+
+// create the Vuex instance by combining the state and mutations objects
+// then export the Vuex store for use by our components
+export default new Vuex.Store({
+  state,
+  mutations,
+});
